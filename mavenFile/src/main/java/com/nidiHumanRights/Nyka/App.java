ed51f3ba784e7d82cde4a99aec264e59a8a7de35
bnidish <nidishbanala007@gmail.com>
2019-11-05 15:24:49 +0530

    Signed-off-by: bnidish <nidishbanala007@gmail.com>

---
+++ mavenFile/src/main/java/com/nidiHumanRights/Nyka/App.java
@@ -0,0 +1,13 @@
+package com.nidiHumanRights.Nyka;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ mavenFile/src/main/java/com/nidiHumanRights/Nyka/Hello3.java
@@ -0,0 +1,7 @@
+class Hello3
+{
+	public static void main(String arg[])
+	{
+		System.out.println("Hello world from class3");
+	}
+}
