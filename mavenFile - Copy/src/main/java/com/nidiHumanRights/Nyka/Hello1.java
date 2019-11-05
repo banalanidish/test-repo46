a8df2d8190c3d2b13fc1088aee03ff881e8787e2
bnidish <nidishbanala007@gmail.com>
2019-11-05 22:54:57 +0530

    mr.banala file has been added
    Signed-off-by: bnidish <nidishbanala007@gmail.com>

---
+++ mavenFile - Copy/src/main/java/com/nidiHumanRights/Nyka/Hello1.java
@@ -0,0 +1,7 @@
+class Hello1
+{
+	public static void main(String arg[])
+	{
+		System.out.println("Hello world from class1");
+	}
+}
---
+++ mavenFile - Copy/src/main/java/com/nidiHumanRights/Nyka/Hello2.java
@@ -0,0 +1,7 @@
+class Hello2
+{
+	public static void main(String arg[])
+	{
+		System.out.println("Hello world from class2");
+	}
+}
